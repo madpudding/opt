b98aad9254ff8fe244c7f6bad2e8b45473122dfd
madpudding <1406508236@qq.com>
2023-10-13 10:42:09 +0800

    add sys out

--- src/main/java/com/opt/license/OptApplication.java
+++ src/main/java/com/opt/license/OptApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OptApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(OptApplication.class, args);
+		System.out.println("Application Started");
 	}
 
 }
